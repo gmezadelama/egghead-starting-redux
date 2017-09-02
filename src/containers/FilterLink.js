885b8b81504e50c8bdc644212ce1279254956b95
GUSTAVO ADOLFO MEZA DE LAMA <GUSTAVO@GUSTAVOs-MacBook-Pro.local>
2017-09-02 19:06:06 -0400

    v29 - Using connect for generating FilterLink container.

--- src/containers/FilterLink.js
+++ src/containers/FilterLink.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
 const Link = ({
     active,
@@ -20,39 +21,39 @@ const Link = ({
   );
 };
 
-export default class FilterLink extends Component {
-  componentDidMount () {
-    const { store } = this.context
-    this.unsubscribe = store.subscribe(() => this.forceUpdate())
-  }
-
-  componentWillUnmount () {
-    this.unsubscribe()
-  }
-
+class FilterLink extends Component {
   render () {
-    const { store } = this.context
-    const props = this.props
-    const state = store.getState()
+    let {
+      active,
+      onClick,
+      children
+    } = this.props
     return (
       <Link
-        active={
-          props.filter ===
-          state.visibilityFilter
-        }
-        onClick={() =>
-          store.dispatch({
-            type: 'SET_VISIBILITY_FILTER',
-            filter: props.filter
-          })
-        }
+        active={ active }
+        onClick={ onClick }
       >
-        {props.children}
+        {children}
       </Link>
     )
   }
 }
 
-FilterLink.contextTypes = {
-  store: React.PropTypes.object
+const mapStateToProps = (state, ownProps) => {
+  return {
+    active: ownProps.filter === state.visibilityFilter
+  }
 }
+
+const mapDispatchToProps = (dispatch, ownProps) => {
+  return {
+    onClick: () => {
+      dispatch({
+        type: 'SET_VISIBILITY_FILTER',
+        filter: ownProps.filter
+      })
+    }
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(FilterLink)
