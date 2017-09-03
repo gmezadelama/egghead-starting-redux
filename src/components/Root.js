646c747817afafbe3cd4a8336ab3f7c40a02deaf
GUSTAVO ADOLFO MEZA DE LAMA <GUSTAVO@GUSTAVOs-MacBook-Pro.local>
2017-09-02 22:05:19 -0400

    Adding React Router v4 to the project

--- src/components/Root.js
+++ src/components/Root.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import { Provider } from 'react-redux'
+import { Router, Route } from 'react-router'
+import { createBrowserHistory } from 'history'
 import TodoApp from '../TodoApp';
 import '../index.css';
 
+const browserHistory = createBrowserHistory()
+
 const Root = ({ store }) => (
   <Provider store = { store }>
-    <TodoApp />
+    <Router history={ browserHistory }>
+      <Route path="/" component={ TodoApp } />
+    </Router>
   </Provider>
 )
 
